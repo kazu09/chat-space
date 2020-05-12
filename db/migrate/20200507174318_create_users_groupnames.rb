2f785fae36c91e52339abf6b9f424ffa13546cf7
kazu09 <kita.k50902@gmail.com>
2020-05-12 20:30:54 +0900

    終了しました

--- db/migrate/20200507174318_create_users_groupnames.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersGroupnames < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users_groupnames do |t|
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,55 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507193643) do
-
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title"
-    t.string   "author"
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  create_table "groupnames", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "groupnames", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "massages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",          limit: 65535
-    t.string   "image"
-    t.integer  "users_id"
-    t.integer  "groupnames_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.index ["groupnames_id"], name: "index_massages_on_groupnames_id", using: :btree
-    t.index ["users_id"], name: "index_massages_on_users_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20200512074517) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                 null: false
-    t.string   "password",             null: false
+    t.string   "name",                                null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "email"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name", "email"], name: "index_users_on_name_and_email", using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "users_groupnames", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "groupnames_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["groupnames_id"], name: "index_users_groupnames_on_groupnames_id", using: :btree
-    t.index ["user_id"], name: "index_users_groupnames_on_user_id", using: :btree
-  end
-
-  add_foreign_key "massages", "groupnames", column: "groupnames_id"
-  add_foreign_key "massages", "users", column: "users_id"
-  add_foreign_key "users_groupnames", "groupnames", column: "groupnames_id"
-  add_foreign_key "users_groupnames", "users"
 end
