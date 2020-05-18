0a305bfe370c187efc7eb693adca04cfabd63155
kazu09 <kita.k50902@gmail.com>
2020-05-18 10:23:19 +0900

    訂正しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    # if @message.save
-    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    # else
     if @message.save
       respond_to do |format|
         format.json
